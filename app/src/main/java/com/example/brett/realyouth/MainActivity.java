e6fe892c1aab98ec4fff82cb1361c1ffe1ed356d
Brett <brettsalyer@gmail.com>
2017-11-14 23:01:58 -0500

    More code cleanup, still not funcitonal. NullPointerErrors in Streaming remain

--- app/src/main/java/com/example/brett/realyouth/MainActivity.java
+++ app/src/main/java/com/example/brett/realyouth/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.brett.realyouth;
 
+import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Handler;
@@ -13,93 +14,28 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.lang.invoke.MutableCallSite;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 
-    public class MainActivity extends AppCompatActivity {
-        private ImageButton playButton;
-        Streaming strm = new Streaming();
-        //private SeekBar musicSeek = strm.getMusicSeek();
-        //private TextView currentTime = strm.getCurrentTime();
-        //private TextView totalTime = strm.getTotalTime();
-        //private MediaPlayer mp = strm.getMp();
+public class MainActivity extends AppCompatActivity {
 
-
+        Streaming stream = new Streaming(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        strm.getMusicSeek().findViewById(R.id.music_seek);
-        strm.getCurrentTime().findViewById(R.id.currentTime);
-        strm.getTotalTime().findViewById(R.id.totalTime);
-        strm.getMp().setAudioStreamType(AudioManager.STREAM_MUSIC);
-        //musicSeek = (SeekBar) findViewById(R.id.music_seek);
-        //currentTime = (TextView)findViewById(R.id.currentTime);
-        //totalTime = (TextView) findViewById(R.id.totalTime);
-       //mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        strm.updateSeekBar();
-        onPlayClick();
-        onDrag();
+        stream.updateSeekBar();
+        stream.onPlayClick();
+        stream.onDrag();
 
     }
 
-    //What happens when the user interacts with the button
-    public void onPlayClick (){
-        playButton = (ImageButton) findViewById(R.id.playButton);
-        try {
-            strm.getMp().setDataSource("http://tricountynaz.net/media/audio/2017-11-08-The%20Compassion%20of%20the%20Christ.mp3");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            strm.getMp().prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        playButton.setOnClickListener(new View.OnClickListener() {
 
-                    @Override
-                    public void onClick(View view) {
-                        playButton.setImageResource(R.drawable.ic_pause_name);
-
-
-                        if (strm.getMp().isPlaying()) {
-                            strm.getMp().pause();
-                            playButton.setImageResource(R.drawable.ic_play_name);
-
-
-                        } else {
-                            //mediaPlayer.start();
-                            strm.getMp().start();
-                        }
-                    }
-                });
-    }
-    //Handles when the user interacts with the seekbar
-    private void onDrag(){
-    strm.getMusicSeek().setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if(b) {
-                    //seeks to the current position of the slider when the user (b) interacts.
-                    strm.getMp().seekTo(i*1000);
-                    strm.getMusicSeek().setProgress(i);
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });}
 
 
 
--- app/src/main/java/com/example/brett/realyouth/Streaming.java
+++ app/src/main/java/com/example/brett/realyouth/Streaming.java
@@ -1,30 +1,60 @@
 package com.example.brett.realyouth;
 
+import android.app.Activity;
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+
 /**
  * Created by brett on 11/14/17.
  */
 
-public class Streaming {
+public class Streaming extends AppCompatActivity {
 
     private SeekBar musicSeek;
     private TextView currentTime;
     private TextView totalTime;
+    private ImageButton play_pause;
     private Handler seekHandler = new Handler();
-    private MediaPlayer mp = new MediaPlayer();;
+    private MediaPlayer mp = new MediaPlayer();
+    Context context;
     Utilities util = new Utilities();
 
+    //Default Constructor
+    public Streaming(){}
+    //Contructor
+    public Streaming(Context context){
+        this.context = context;
+    }
     //Method to run the runnable to update the seekbar
     public void updateSeekBar(){
         seekHandler.postDelayed(mUpdateTimeTask, 100);
+    }
+
+    public void prepareStreaming() throws IOException {
+        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mp.setDataSource("http://tricountynaz.net/media/audio/2017-11-08-The%20Compassion%20of%20the%20Christ.mp3");
+        mp.prepare();
+    }
+
+    public void startStreaming(){
+        mp.start();
+    }
+
+    public void pauseStreaming(){
+        mp.pause();
     }
 
     //Runnable to update the seekbar with the current position.
@@ -35,10 +65,13 @@ public class Streaming {
 
 
             //Displaying Total Duration time
+            totalTime = (TextView)((Activity)context).findViewById(R.id.totalTime);
             totalTime.setText(util.milliSecondsToTimer(totalDuration));
             // Displaying time completed playing
+            currentTime = (TextView)((Activity)context).findViewById(R.id.currentTime);
             currentTime.setText(util.milliSecondsToTimer(currentPosition));
             //Set the bars total duration, based on the song duration (converted to seconds)
+            musicSeek = (SeekBar)((Activity)context).findViewById(R.id.music_seek);
             musicSeek.setMax(totalDuration/1000);
             // Updating progress bar
             musicSeek.setProgress(mp.getCurrentPosition()/1000);
@@ -47,6 +80,57 @@ public class Streaming {
             seekHandler.postDelayed(this, 100);
         }
     };
+
+    //What happens when the user interacts with the button
+    public void onPlayClick (){
+        play_pause = (ImageButton)((Activity)context).findViewById(R.id.playButton);
+        play_pause.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                play_pause.setImageResource(R.drawable.ic_pause_name);
+
+                try {
+                    prepareStreaming();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (mp.isPlaying()) {
+                    pauseStreaming();
+                    play_pause.setImageResource(R.drawable.ic_play_name);
+
+
+                } else {
+                    //mediaPlayer.start();
+                    startStreaming();
+                }
+            }
+        });
+    }
+
+    //Handles when the user interacts with the seekbar
+    public void onDrag(){
+        musicSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if(b) {
+                    //seeks to the current position of the slider when the user (b) interacts.
+                    mp.seekTo(i*1000);
+                    musicSeek.setProgress(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });}
     public TextView getCurrentTime() {
         return this.currentTime;
     }
