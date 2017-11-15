1e83386f27c39520f09fbf36350ec01c89ee5d7b
Brett <brettsalyer@gmail.com>
2017-11-15 15:50:40 -0500

    Corrected incorrect state error. PlayButton not functional, yet

--- app/src/main/java/com/example/brett/realyouth/MainActivity.java
+++ app/src/main/java/com/example/brett/realyouth/MainActivity.java
@@ -28,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            stream.prepareStreaming();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stream.startStreaming();
+
 
         stream.updateSeekBar();
         stream.onPlayClick();
